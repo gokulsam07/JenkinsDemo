059ec73ddaf7c48455cb7063e9f4238f24905775
Gokul <gokuls2381@gmail.com>
2023-05-20 14:37:13 +0530

    Demo commit

---
+++ demoJenkins/src/main/java/automation/demoJenkins/BreakFast.java
@@ -0,0 +1,11 @@
+package automation.demoJenkins;
+
+public class BreakFast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ate breakfast");
+
+	}
+
+}
---
+++ demoJenkins/src/main/java/automation/demoJenkins/WakeUp.java
@@ -0,0 +1,10 @@
+package automation.demoJenkins;
+
+public class WakeUp {
+
+	public static void main(String[] args) {
+		System.out.println("Woke Up");
+
+	}
+
+}
